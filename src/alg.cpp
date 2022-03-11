ffb485c49f83a642cc3a73673dcd49202c70eaab
OdintsovDanil <99181188+OdintsovDanil@users.noreply.github.com>
2022-03-11 23:12:05 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,6 +1,27 @@
 // Copyright 2021 NNTU-CS
 
 int cbinsearch(int *arr, int size, int value) {
-  //  поместить сюда реализацию алгоритма
-  return 0; // если ничего не найдено
+  int count = 0, left = 0, right = size - 1, mid;
+    while (left < right) {
+        mid = (left + right) / 2;
+        if (arr[mid] == value) {
+            left = mid;
+            while (arr[left] == value) {
+                left--;
+            }
+            while (arr[right] != value) {
+                right--;
+            }
+            for (int i = left + 1; i < right; i++) {
+                count++
+            }
+        }
+        if (arr[mid] < value) {
+            left = mid + 1;
+        }
+        if (arr[mid] > value) {
+            right = mid - 1;
+        }
+    }
+    return count
 }
